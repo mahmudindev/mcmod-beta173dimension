413b433609bfe3f034f8a7363043c66b62979694
M. Mahmudin <42290713+mahmudindev@users.noreply.github.com>
2025-07-29 20:31:53 +0700

    Updates to 1.21

---
+++ neoforge/src/main/java/com/github/mahmudindev/mcmod/beta173dimension/neoforge/Beta173DimensionNeoForge.java
@@ -0,0 +1,12 @@
+package com.github.mahmudindev.mcmod.beta173dimension.neoforge;
+
+import com.github.mahmudindev.mcmod.beta173dimension.Beta173Dimension;
+import net.neoforged.fml.common.Mod;
+
+@Mod(Beta173Dimension.MOD_ID)
+public final class Beta173DimensionNeoForge {
+    public Beta173DimensionNeoForge() {
+        // Run our common setup.
+        Beta173Dimension.init();
+    }
+}
